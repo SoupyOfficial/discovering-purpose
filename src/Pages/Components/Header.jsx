6924bcbc1de6f65f844a13d21c1ff86ddd8e2da2
Jacob Campbell <59628901+SoupyOfficial@users.noreply.github.com>
2023-10-09 13:18:14 -0400

    UI Updates

--- src/Pages/Components/Header.jsx
+++ src/Pages/Components/Header.jsx
@@ -2,48 +2,9 @@ import React, { useState, useEffect } from 'react';
 import './Styles/Header.css';
 
 const Header = () => {
-  // Attempt 1
-  const [backgroundColor, setBackgroundColor] = useState('blue');
-
-  // Function to calculate the background color based on scroll position
-  const calculateBackgroundColor = () => {
-    const windowHeight = window.innerHeight;
-    const scrollPosition = window.scrollY;
-    const gradientColor = calculateGradientColor(scrollPosition / windowHeight);
-    setBackgroundColor(gradientColor);
-  };
-
-  // Function to calculate gradient color based on a percentage value
-  const calculateGradientColor = (percentage) => {
-    // Replace these with your specific gradient colors
-    const gradientColor1 = 'rgba(255, 0, 0, 1)'; // Replace with your color
-    const gradientColor2 = 'rgba(0, 0, 255, 1)'; // Replace with your color
-
-    // Calculate the interpolated color based on the percentage
-    const r = Math.floor(
-      parseInt(gradientColor1.slice(4, 7)) * percentage +
-        parseInt(gradientColor2.slice(4, 7)) * (1 - percentage)
-    );
-    const g = Math.floor(
-      parseInt(gradientColor1.slice(9, 12)) * percentage +
-        parseInt(gradientColor2.slice(9, 12)) * (1 - percentage)
-    );
-    const b = Math.floor(
-      parseInt(gradientColor1.slice(14, 17)) * percentage +
-        parseInt(gradientColor2.slice(14, 17)) * (1 - percentage)
-    );
-    
-    console.log(r, g, b)
-
-    return `rgba(${r}, ${g}, ${b}, 1)`;
-  };
 
   return (
-    <header className="header-content" 
-      style={{
-          // backgroundColor: `${backgroundColor}`,
-          // opacity: scrollY > 10 ? 1 : 0.5,
-        }}>
+    <header className="header-content">
       <div className="logo">
         <img src="https://placehold.co/40x20" alt="Logo" />
       </div>
@@ -51,8 +12,9 @@ const Header = () => {
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#services">Services</a></li>
-        <li><a href="#research">Research</a></li>
         <li><a href="#contact">Contact</a></li>
+        <li><a href="#research">Research</a></li>
+        <li><a href="#insurance">Insurance</a></li>
       </ul>   
     </header>
   );
--- src/Pages/Insurance.jsx
+++ src/Pages/Insurance.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Styles/Services.css';
 
-const Services = () => {
+const Insurance = () => {
   return (
-    <section id="services" className="services-section">
+    <section id="insurance" className="services-section">
       <div className="services-text">
         <h2>Insurance Accepted</h2>
         <p>Currently, Lori uses a combination of approaches, such as CBT, to help her clients manage moderate symptoms quickly; psychoanalysis, to help her clients understand where their disordered thoughts stem from; DBT, to help her higher level clients reduce their severe symptoms over time through mindfulness techniques, distress tolerance skills, emotional regulation, and interpersonal effectiveness; and a multitude of other integrative treatment approaches to best meet her client’s needs. Lori currently works with ages 13+, and is happy to offer a Christian counseling perspective upon request.</p>
@@ -12,4 +12,4 @@ const Services = () => {
   );
 };
 
-export default Services;
+export default Insurance;
